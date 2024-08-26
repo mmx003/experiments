04ade5a62fa3cdac7a20dd7a28964679a8f81e9f
mmx003 <mmx003@gmail.com>
2024-08-26 15:23:18 +0300

    protobuf sample

---
+++ Researches/Protobuff/WebApplication1/Program.cs
@@ -0,0 +1,66 @@
+using System.Text;
+using ProtoBuf;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.MapGet("/test", () =>
+{
+    var x = new Person()
+    {
+        Id = 1,
+        Address = new Address() { Id = 0, Line2 = "str. Larionova 41/4" },
+        Name = "Max T",
+        CurrentDate = DateTime.Now,
+        PersonGuid = Guid.NewGuid().ToString()
+    };
+    var ms = new MemoryStream();
+    Serializer.Serialize(ms, x);
+    var eTxt = Encoding.Default.GetString(ms.ToArray());
+
+    return ms.ToArray();
+    // return "Ok";
+});
+
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
